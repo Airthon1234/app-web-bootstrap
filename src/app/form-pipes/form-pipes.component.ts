1fa10cb52f1ba6830c2bf994d05b5c7736bfc273
Airthon1234 <cipoteland@gmail.com>
2024-06-21 17:53:18 -0500

    typescript de ejercicio pipe

--- src/app/form-pipes/form-pipes.component.ts
+++ src/app/form-pipes/form-pipes.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule} from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-form-pipes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './form-pipes.component.html',
   styleUrl: './form-pipes.component.css'
 })
 export class FormPipesComponent {
-
+  alumnos = [
+    {nombre: "Luis angel"},
+    {nombre: "Luis alberto"},
+    {nombre: "Luis Raúl"},
+    {nombre: "Luis Gabriel"}
+  ]
+  fechaActual=new Date()
+  cursos = [
+    {nombre: "Desarrollo de Aplicacion web", precio: 3200},
+    {nombre: "Desarrollo de Servicio web", precio:2500},
+    {nombre: "Base de datos 2", precio: 2850},
+    {nombre: "Ingles tecnico", precio: 1850},
+  ]
 }
